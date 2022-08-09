8beda6b539e411c64b3c0635a75269c9f369c5ac
Migdalis <miglepage@gmail.com>
2022-08-08 23:19:36 -0500

    :white_check_mark: Fix task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """ Async routine """
-import asyncio
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
         Function that spawn wait_random n times with
         the specified max_delay, and return  the list of all the delays
