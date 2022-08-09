edcb9037594c642dbcbfac5ae9e4eb43f5715665
Migdalis <miglepage@gmail.com>
2022-08-08 22:39:12 -0500

    :white_check_mark: Fix task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,5 +9,6 @@ async def wait_random(max_delay: int = 10) -> float:
         Asynchronous coroutine that takes in an integer argument
         and waits for a random delay between 0 and max_delay
     """
-    await asyncio.sleep(max_delay)
-    return (random.uniform(0, max_delay))
+    rand_num = random.uniform(0, max_delay)
+    await asyncio.sleep(rand_num)
+    return (rand_num)
